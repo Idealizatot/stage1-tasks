cbed4e5b8981240f8bfcadb52fbc758637529a77
Idealizatot <ideal103.5@gmail.com>
2021-03-11 16:05:31 +0300

    init: start virtual-piano

---
+++ virtual-piano/script.js
@@ -0,0 +1,81 @@
+const playNote = (noteName) => {
+	let sound = new Audio(`assets/audio/${noteName}.mp3`)
+	sound.play();
+} 
+
+const checkClickMouse = event => {
+	const noteName = event.target.dataset.note
+	playNote(noteName, event)
+	event.target.classList.add('piano-key-active')
+}
+
+const checkDownButton = (event) => {
+	const btnName = event.code[event.code.length - 1]
+	const noteButton = document.querySelector(`[data-letter="${btnName}"]`)
+	
+	if (!noteButton) {
+		return;
+	}
+
+	if (event.repeat) {
+		return
+	}
+
+	const noteName = noteButton.dataset.note
+	playNote(noteName, event)
+	noteButton.classList.add('piano-key-active')
+}
+
+const checkUpButton = event => {
+	const btnName = event.code[event.code.length - 1]
+	const noteButton = document.querySelector(`[data-letter="${btnName}"]`)
+	if (!noteButton) {
+		return;
+	}
+	noteButton.classList.remove('piano-key-active')
+}
+
+const pianoButtons = document.querySelectorAll('.piano-key')
+pianoButtons.forEach(element => {
+	element.addEventListener('mousedown', (event) => {
+		checkClickMouse(event)
+	})
+	element.addEventListener('mouseup', (event) => {
+		event.target.classList.remove('piano-key-active')
+	})
+	element.addEventListener('mouseout', (event) => {
+		event.target.classList.remove('piano-key-active')
+	})
+})
+
+window.addEventListener('keydown', checkDownButton)
+window.addEventListener('keyup', checkUpButton)
+
+const deleteActive = () => {
+	let buttons = document.querySelectorAll('.btn-container>button')
+	buttons.forEach(element => {
+		element.classList.remove('btn-active')
+	})
+} 
+
+const notesButton = document.getElementById('notes');
+notesButton.addEventListener('click', () => {
+	pianoButtons.forEach(element => {
+		element.classList.remove('piano-key-letters')
+		element.classList.add('piano-key-notes')
+	})
+	deleteActive()
+	notesButton.classList.add('btn-active')
+})
+
+const lettersButton = document.getElementById('letters');
+lettersButton.addEventListener('click', () => {
+	pianoButtons.forEach(element => {
+		element.classList.remove('piano-key-notes')
+		element.classList.add('piano-key-letters')
+		element.classList.add('btn-active')
+	})
+	deleteActive()
+	lettersButton.classList.add('btn-active')
+})
+
